69845420f100426eb2a931a2306cc3fb678f6a4d
Mind <nathapat.mind@gmail.com>
2023-02-21 16:43:05 +0700

    Lab20_Final1

--- lab20_1.cpp
+++ lab20_1.cpp
@@ -3,10 +3,32 @@ using namespace std;
 
 struct Rect{
 	double x,y,w,h;
+	
 };
 
-double overlap(){
+double overlap(Rect R1,Rect R2){
+    double result,yt,yb,xl,xr;
+    if(R1.y < R2.y){
+        yt = R1.y;
+    }else yt = R2.y;
+    
+    if(R1.y - R1.h > R2.y - R2.h){
+        yb = R1.y - R1.h;
+    }else yb = R2.y - R2.h;
+    
+    if(R1.x > R2.x){
+        xl = R1.x;
+    }else xl = R2.x;
+    
+    if(R1.x + R1.w < R2.x + R2.w){
+        xr = R1.x + R1.w;
+    }else xr = R2.x + R2.w;
 
+    result = (yt-yb)*(xr-xl);
+    if(result > 0) return result;
+    else return 0;
+    
+    
 
 
 }
